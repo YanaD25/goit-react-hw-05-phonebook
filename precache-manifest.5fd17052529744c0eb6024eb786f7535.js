4ea119c12422f3292ec588c6f508674522b5e5b0
YanaD25 <yanaderevianko25@gmail.com>
2020-11-15 23:39:25 +0200

    Updates

---
+++ precache-manifest.5fd17052529744c0eb6024eb786f7535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d650605c340523fbec7a0227d713cb8",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "4ddca6f0cf05b48f8ec8",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.6497563b.chunk.css"
+  },
+  {
+    "revision": "52d0476380cf59ea83d3",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.e6fe4ed4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.e6fe4ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddca6f0cf05b48f8ec8",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.857babbf.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
